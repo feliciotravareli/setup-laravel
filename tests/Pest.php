0e91a844f15612ecc5ef80692f430304f918177f
Felicio Travareli <felicio.travareli@gmail.com>
2022-03-16 11:12:22 -0300

    phpcs fix file

--- tests/Pest.php
+++ tests/Pest.php
@@ -15,7 +15,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 uses(
     Tests\TestCase::class,
     RefreshDatabase::class,
-    )->in('Feature');
+)->in('Feature');
 
 /*
 |--------------------------------------------------------------------------
