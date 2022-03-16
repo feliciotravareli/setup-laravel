c6c8ec72f4dd3d8ec536134aa34d90c627d32f4b
Felicio Travareli <felicio.travareli@gmail.com>
2022-03-15 22:41:31 -0300

    php-cs-fixer

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    User::query()
+        ->where('id', '>', 1)
+        ->get();
+
     return view('welcome');
 });
